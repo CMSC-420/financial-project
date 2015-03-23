2b53c629e65e7090876918be1762a1a3c2563936
Adam <johnsonam23@vcu.edu>
2015-03-23 10:29:23 -0400

    reorganized code

--- GUI.java
+++ GUI.java
@@ -26,61 +26,36 @@ import java.io.*;
 import java.util.*;
 public class GUI {
 	
-	/**
-	 * 
-	 * GUI Components
-	 */
-	
 	//Basics for every GUI 
 	static JFrame frame;
 	private static JPanel panel;
-    private static int windowHeight;
-    private static int windowWidth;
-    private static GroupLayout groupLayout;
-	
-	
-	//Buttons for user to select the option they wish
-	static JButton act_mgmt, reports, record_transaction, button_1, button_2;
     
-	/**
-	 * 
-	 *Below defines the components background data;
-	 * @param args
-	 */
-	
-	
-	/**Elements of the ComboBox
-	*  not implemented but may be a better solution in the future
-	*  static String[] view_options = { "Account Management", "Reports", "Record Transactions"};
-	*/
-	
-	//User selectable drop down menu to select the report the wish to view
-	private static JComboBox view_acct;
-
-	
-	//used for selection management --> Will be used later on during the project
-	private ListSelectionModel listModel; 
-	//scrollpane for table implementation --> give the table a scrollbar after the limit for viewable entries has be meet meet
+    private static int windowHeight; // current height of the window
+    private static int windowWidth; // current width of the window
+    private static GroupLayout groupLayout; // the layout for the components
+    
+	private static JButton act_mgmt, reports, record_transaction, button_1, button_2; // Buttons
+	private static JComboBox view_acct; // User selectable drop down menu to select the account they wish to view
+ 
+	// scrollpane for table implementation --> give the table a scrollbar after the limit for viewable entries has been met
 	private static JScrollPane scrollPane;
 	private static JTable table;
+    private static MyTableModel tableModel;
 	
-	
-
-
-	
-	
-	
+    
+    
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Defines and sets up the Frame and Panel
+		
+        // Defines and sets up the Frame and Panel
 		// loads the GUI
 		GUI();
-		
 		
 	} // main
     
     
 	
+    
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void GUI(){
 	
@@ -88,17 +63,19 @@ public class GUI {
 		// sets the size of the frame
 		frame.setSize(1024,768);   // <-- Feel free change this dimensions accordingly for debugging --> My machine current aspect ratio is 1600 x 900
 		
-		//creates a table for user date entry -->table and table model implemented later in cod
+		//creates a table for user date entry
 		table = new JTable();
+        tableModel = new MyTableModel();
 		//give table the ability to select multiple rows simultaneously
 		table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION); 
-		table.setPreferredSize(new Dimension(400,300)); // <-- Feel free change this dimensions accordingly for debugging --> My machine current aspect ratio is 1600 x 900
+		//table.setPreferredSize(new Dimension(400,300)); // <-- Feel free change this dimensions accordingly for debugging --> My machine current aspect ratio is 1600 x 900
 		table.setFillsViewportHeight(true); // the table fills out the JScrollPane
         table.setAutoResizeMode(1); // table till auto-resize
+        table.setModel(new MyTableModel());
 		
 		//scrollpane --> gives the table a scrollbar when the the table entries go outside the space defined for the table on the Frame 
 		scrollPane = new JScrollPane();
-		scrollPane.setPreferredSize(new Dimension(400,400)); //<-- Feel free change this dimensions accordingly for debugging --> My machine current aspect ratio is 1600 x 900
+		//scrollPane.setPreferredSize(new Dimension(400,400)); //<-- Feel free change this dimensions accordingly for debugging --> My machine current aspect ratio is 1600 x 900
 		//adds the table to the scrollpane
 		scrollPane.setColumnHeaderView(table);
 		
@@ -111,8 +88,11 @@ public class GUI {
 		*/
 		view_acct= new JComboBox<String>();
         
-        // makes the X in the titlebar close the program
-        // DO NOT DELETE THIS AGAIN
+        /**
+         * makes the X in the titlebar close the program
+         * DO NOT DELETE THIS AGAIN
+         * Without it the window will close but the program will still be running.
+         */
         frame.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e){
 				System.exit(0);
@@ -133,18 +113,11 @@ public class GUI {
 		 * 		3) Record Transaction
 		 * 
 		 * 
-		 * Drop down menu for the user to select the type of report they wish to view
-		 * 		1) Account Management
-		 * 		2) Reports
-		 * 		3) Record Transactions
-		 * 
-		 * 
+		 * Drop down menu for the user to select the account they wish to view
 		 *
 		 */
 		
 		
-		
-		//Start Selectable Button
 		// account management
 		act_mgmt =  new JButton("Accounts");
 		act_mgmt.addActionListener(new ActionListener() {
@@ -182,43 +155,9 @@ public class GUI {
 				
 				
 			}
-		});  //end user selectable buttons
+		});
 		
-		
-		
-		
-		
-		
-		
-		
-		/**adds components to the Drop down menu for user selectability --> Can be implemented in an Array as seen above: 
-		 * 																	This solution could be a better solution in the long run as it allows for a single array to contain
-		 * 																	all the user options and then access them with the correct array index --> would make code more organized
-		*																	but the current implementation will work for the current sprint
-		*/
-		view_acct.addItem("Account 1");
-		view_acct.addItem("Account 2");
-		view_acct.addItem("Account 3");
-		
-		
-		
-		/**
-		 * 
-		 * Below Creates a table and a table model:
-		 * 	--> The Table allows for data entry
-		 *  --> The Table model allows a custom model in order to implement a table with selectable and editable cell --> Like an excel spreadsheet 
-		 * 
-		 * 
-		 */
-	
-		table.setModel(new MyTableModel());
-		
-		/**
-		 * 
-		 * Bellow are the buttons you requested
-		 * 
-		 */
-		
+        
 		//button 1
 		button_1 = new JButton("Button 1");
 		button_1.addActionListener(new ActionListener() {
@@ -237,35 +176,21 @@ public class GUI {
 				
 				//code for onclick event goes here
 			}
-		});//end requested buttons
-		
-		
-		/**
-		 * 
-		 * 
-		 * 	Below Provides positioning definitions for all GUI components:
-		 * 
-		 *  	1)User selectable Buttons:  
-		 *  		1) Account Management
-		 * 			2) Reports
-		 * 			3) Record Transaction
-		 * 		2) Drop Down Box for user selectable report:
-		 * 			1) Account Management
-		 * 			2) Reports
-		 * 			3) Record Transaction
-		 * 		3) Table and MyTableModel
-		 * 		4) "Button 1" and "Button 2" --> Adam please update the names once you name them
-		 * 		________________________________________________________________________________________
-		 * 
-		 * 		This list will be updated as components are added to the project
-		 * 
-		 * 
-		 */
-         
+		});
+        
+        
+        // adds components to the Drop down menu for user to select the account they wish to view
+		view_acct.addItem("Account 1");
+		view_acct.addItem("Account 2");
+		view_acct.addItem("Account 3");
+        
+        
+        
+        // These define the current height and width of the window.
         windowHeight = frame.getBounds().height;
         windowWidth = frame.getBounds().width;
 		
-		
+		// setup the layout
         groupLayout = new GroupLayout(frame.getContentPane());
 		makeLayout();
 		
@@ -273,11 +198,12 @@ public class GUI {
 		
 		/**
 		 * 
-		 * What ever you do, DO NOT Delete the below line of code: This line sets the GUI FRAME and ALL SUB COMPONENTS to visible. 
+		 * What ever you do, DO NOT DELETE the below line of code: This line sets the GUI FRAME and ALL SUB COMPONENTS to visible. 
 		 * 
-		 * Without this line, if you run the application, NOTHINg will display and it will seem as if something is broken. 
+		 * Without this line, if you run the application, NOTHING will display and it will seem as if something is broken. 
 		 * This is not the case, the application IS running it is simply not showing thus why you must set the visibility to TRUE.
 		 *
+         * Also makes the layout resize itself when the window is resized.
 		 */
         frame.getContentPane().setLayout(groupLayout);
         frame.addComponentListener(new ResizeListener());
@@ -287,6 +213,9 @@ public class GUI {
 		
 		
 	} // GUI
+    
+    
+    
     
     // draw the components onto the window
     private static void makeLayout(){
@@ -335,9 +264,12 @@ public class GUI {
                     )
                 )
         );
-        groupLayout.linkSize(SwingConstants.HORIZONTAL, act_mgmt, reports, record_transaction, button_1, button_2);
+        // keep the buttons all the same size
+        groupLayout.linkSize(SwingConstants.HORIZONTAL, act_mgmt, reports, record_transaction, view_acct, button_1, button_2);
         groupLayout.linkSize(SwingConstants.VERTICAL, act_mgmt, reports, record_transaction, view_acct, button_1, button_2);
     } // makeLayout
+    
+    
     
     
     // update the layout whenever the window is resized
@@ -353,6 +285,8 @@ public class GUI {
         }
     } // class ResizeListener
 	
+    
+    
     
     /*
 	 * This class was made to allow the use of certain variable types in the table.
