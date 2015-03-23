02bfae9f79470a257429d03f1fdf4aa33b59a09d
Adam <johnsonam23@vcu.edu>
2015-03-23 11:05:10 -0400

    added type variable to Account

--- Account.java
+++ Account.java
@@ -7,6 +7,7 @@
 
 public class Account{
     
+    protected String type; // what type of account is this?
     private int balance; // balance of the account
     private String name; // name of the account
     
@@ -36,32 +37,52 @@ public class Account{
     } // getName
 } // Account class
 
+
+
+
 class Checking extends Account{
     public Checking(){
-        
+        super();
+        type = "Checking";
     } // default constructor
 } // Checking class
+
+
+
 
 class Savings extends Account{
     public Savings(){
         super();
+        type = "Savings";
     } // default constructor
 } // Savings class
+
+
+
 
 class CreditCard extends Account{
     public CreditCard(){
         super();
+        type = "CreditCard";
     } // default constructor
 } // CreditCard class
+
+
+
 
 class COD extends Account{
     public COD(){
         super();
+        type = "COD";
     } // default constructor
 } // COD class
+
+
+
 
 class MoneyMarket extends Account{
     public MoneyMarket(){
         super();
+        type = "MoneyMarket";
     } // default constructor
 } // MoneyMarket class
